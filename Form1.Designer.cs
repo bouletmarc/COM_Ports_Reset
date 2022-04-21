8c12fe38da373b9c37cd744b32be6f7161053002
Bouletmarc <bouletmarc@hotmail.com>
2022-04-21 07:03:17 -0400

    V1.0 - Initial Release

---
+++ Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace COMPortReseter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.CheckOnClick = true;
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(12, 46);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(283, 619);
+            this.checkedListBox1.TabIndex = 0;
+            this.checkedListBox1.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler(this.checkedListBox1_ItemCheck);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(35, 670);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Cancel";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(184, 670);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Save";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Blue;
+            this.label1.Location = new System.Drawing.Point(31, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(242, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "SET or RESET COM Ports IN USE";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(308, 700);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkedListBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "COM Ports Reseter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using Microsoft.Win32;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO.Ports;
+using System.Windows.Forms;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Reflection;
+using System.Security.Cryptography;
+using System.Security.Permissions;
+using System.Security.Principal;
+using System.Management;
+
+namespace COMPortReseter
+{
+    public partial class Form1 : Form
+    {
+        public SerialPort serial;
+        public List<string> AvailablePorts = new List<string>();     //Available COM Ports List
+
+        public string RegistryKeyAccess = @"HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\COM Name Arbiter";
+        public string ThisKeyName = "ComDB";
+
+        public byte[] COM_Ports_Bytes = new byte[] { };
+        public byte[] ClearData = new byte[] { 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00 };
+        public byte[] COM_Ports_Bytes_NEW = new byte[] { };
+
+        public byte[] NewByte = new byte[1] { 0x00 };
+
+        public bool LoadingValues = true;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            AdminRelauncher();
+            SpawnList();
+
+            LoadingValues = false;
+        }
+
+        private void AdminRelauncher()
+        {
+            if (!IsRunAsAdmin())
+            {
+                ProcessStartInfo proc = new ProcessStartInfo();
+                proc.UseShellExecute = true;
+                proc.WorkingDirectory = Environment.CurrentDirectory;
+                try
+                {
+                    proc.FileName = Assembly.GetEntryAssembly().CodeBase;
+                }
+                catch
+                {
+                    proc.FileName = Application.ExecutablePath;
+                }
+
+                proc.Verb = "runas";
+
+                try
+                {
+                    Process.Start(proc);
+                }
+                catch { }
+                Environment.Exit(0);
+            }
+        }
+
+        private bool IsRunAsAdmin()
+        {
+            WindowsIdentity id = WindowsIdentity.GetCurrent();
+            WindowsPrincipal principal = new WindowsPrincipal(id);
+
+            return principal.IsInRole(WindowsBuiltInRole.Administrator);
+        }
+
+        public void GetRegistryKey(string KeyName)
+        {
+            COM_Ports_Bytes = (byte[]) Registry.GetValue(RegistryKeyAccess, KeyName, ClearData);
+            if (COM_Ports_Bytes != null)
+            {
+                COM_Ports_Bytes_NEW = COM_Ports_Bytes;
+                int CurrentByte = 0;
+                int CurrentBit = 0;
+                LoadingValues = true;
+                for (int i = 0; i < 40; i++)
+                {
+                   if (GetBit(COM_Ports_Bytes[CurrentByte], CurrentBit))
+                    {
+                        checkedListBox1.SetItemChecked(i, true);
+                    }
+
+                    CurrentBit++;
+                    if (CurrentBit >= 8)
+                    {
+                        CurrentByte++;
+                        CurrentBit = 0;
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("No Value in COM Name Arbiter");
+            }
+        }
+
+        public void SetBit(int ByteNubmer, int bitNumber, bool Activated)
+        {
+            Console.WriteLine("StartByte: " + COM_Ports_Bytes_NEW[ByteNubmer].ToString("X2"));
+            BitArray bitArray = new BitArray(new byte[] {COM_Ports_Bytes_NEW[ByteNubmer]});
+            bitArray.Set(bitNumber, Activated);
+            bitArray.CopyTo(NewByte, 0);
+
+            Console.WriteLine("EndByte: " + COM_Ports_Bytes_NEW[ByteNubmer].ToString("X2"));
+
+            COM_Ports_Bytes_NEW[ByteNubmer] = NewByte[0];
+        }
+
+        public bool GetBit(byte b, int bitNumber)
+        {
+            return (b & (1 << bitNumber)) != 0;
+        }
+
+        public void SetRegistryKey(string KeyName)
+        {
+            try
+            {
+                Registry.SetValue(RegistryKeyAccess, KeyName, COM_Ports_Bytes_NEW, RegistryValueKind.Binary);
+            }
+            catch (Exception mess)
+            {
+                MessageBox.Show("Error setting RegistryKey:" + Environment.NewLine + mess);
+            }
+        }
+
+        public void RegistryDemandPermission()
+        {
+            RegistryPermission permission = new RegistryPermission(RegistryPermissionAccess.AllAccess, RegistryKeyAccess);
+            permission.Demand();
+            permission = null;
+        }
+
+        public void SpawnList()
+        {
+            AvailablePorts.Clear();
+            foreach (string s in SerialPort.GetPortNames())
+            {
+                AvailablePorts.Add(s);
+            }
+
+            checkedListBox1.Items.Clear();
+            LoadingValues = true;
+            for (int i = 0; i < 40; i++)
+            {
+                string COMName = "COM" + i;
+                string BufFinalName = GetPortName(COMName);
+                if (BufFinalName != "")
+                {
+                    checkedListBox1.Items.Add(COMName + " | Connected | " + BufFinalName);
+                }
+                else
+                {
+                    checkedListBox1.Items.Add(COMName);
+                }
+                checkedListBox1.SetItemChecked(i, false);
+            }
+
+            //#########################
+            RegistryDemandPermission();
+            GetRegistryKey(ThisKeyName);
+        }
+
+        public string GetPortName(string ThisCOM)
+        {
+            for (int i = 0; i < AvailablePorts.Count; i++)
+            {
+                if (AvailablePorts[i] == ThisCOM)
+                {
+                    return AvailablePorts[i];
+                }
+            }
+
+            return "";
+        }
+
+        private void checkedListBox1_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            /*if (!LoadingValues)
+            {
+                int CurrentByte = 0;
+                int CurrentBit = 0;
+                for (int i = 0; i < 40; i++)
+                {
+                    SetBit(CurrentByte, CurrentBit, checkedListBox1.GetItemChecked(i));
+
+                    CurrentBit++;
+                    if (CurrentBit >= 8)
+                    {
+                        CurrentByte++;
+                        CurrentBit = 0;
+                    }
+                }
+
+                SetRegistryKey(ThisKeyName);
+
+                SpawnList();    //respawn list
+                LoadingValues = false;
+            }*/
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            COM_Ports_Bytes_NEW = COM_Ports_Bytes;
+
+            RegistryDemandPermission();
+            SetRegistryKey(ThisKeyName);
+            SpawnList();    //respawn list
+            LoadingValues = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!LoadingValues)
+            {
+                int CurrentByte = 0;
+                int CurrentBit = 0;
+                for (int i = 0; i < 40; i++)
+                {
+                    SetBit(CurrentByte, CurrentBit, checkedListBox1.GetItemChecked(i));
+
+                    CurrentBit++;
+                    if (CurrentBit >= 8)
+                    {
+                        CurrentByte++;
+                        CurrentBit = 0;
+                    }
+                }
+
+                RegistryDemandPermission();
+                SetRegistryKey(ThisKeyName);
+
+                SpawnList();    //respawn list
+                LoadingValues = false;
+            }
+        }
+    }
+}
